9108fef2df5ed249bafdf94b09be83b776d771b4
Sakar <sakar.michel@gmail.com>
2020-04-01 19:59:17 +0000

    added backend script setup

---
+++ setupScript/setupScriptForDB.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+
+#sudo apt-get upgrade
+
+sudo apt install php
+
+sudo apt install php-mbstring
+
+cd ~/IT490-2/backEnd
+
+git clone  https://github.com/MattToegel/IT490.git
+
+sudo apt install php-mbstring
+
+sudo apt install composer
+
+composer require php-amqplib/php-amqplib
+
+#sudo apt-get install apache2
