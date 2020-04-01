b0dbc541a30571417239e1c37f7db3226e85caa7
Sakar <sakar.michel@gmail.com>
2020-04-01 18:43:57 +0000

    Setup up script for app

---
+++ setupScript/setupScriptForApp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudp apt-get upgrade
+
+sudo apt install php
+
+sudo apt install php-mbstring
+
+git clone  https://github.com/MattToegel/IT490.git
+
+cd IT490
+
+sudo apt install php-mbstring
+
+sudo apt install composer
+
+composer require php-amqplib/php-amqplib
+
+sudo apt install apache
