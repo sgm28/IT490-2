456eb3398486ce793c39f534bff40eec3819bf99
Sakar <sakar.michel@gmail.com>
2020-04-10 17:35:56 +0000

    Recomments

--- setupScript/setupScriptForDB.sh
+++ setupScript/setupScriptForDB.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-#sudo apt-get update
+sudo apt-get update
 
-#sudo apt-get upgrade
+sudo apt-get upgrade
 
-#sudo apt install php
+sudo apt install php
 
-#sudo apt install php-mbstring
+sudo apt install php-mbstring
 
-#cd ~/IT490-2/backEnd
+cd ~/IT490-2/backEnd
 
-#git clone  https://github.com/MattToegel/IT490.git
+git clone  https://github.com/MattToegel/IT490.git
 
-#sudo apt install php-mbstring
+sudo apt install php-mbstring
 
-#sudo apt install composer
+sudo apt install composer
 
-#composer require php-amqplib/php-amqplib
+composer require php-amqplib/php-amqplib
 
-#sudo apt install php-mysqli;
+sudo apt install php-mysqli;
 
 sudo rm -r '/etc/php/7.2/cli/php.ini'
 
