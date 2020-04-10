eac9f8c343cbc1dea852ff1e580ef8f26eec2407
Sakar <sakar.michel@gmail.com>
2020-04-10 16:16:21 +0000

    Change the names of the column in the table

--- setupScript/setupScriptForDB3.php
+++ setupScript/setupScriptForDB3.php
@@ -19,18 +19,18 @@ if ($conn->connect_error) {
 // sql to create table
 $sql = "CREATE TABLE Users (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-password VARCHAR(50),
-dob VARCHAR(50),
+FirstName VARCHAR(30) NOT NULL,
+LastName VARCHAR(30) NOT NULL,
+Email VARCHAR(50),
+Password VARCHAR(50),
+DOB VARCHAR(50),
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
 if ($conn->query($sql) === TRUE) {
     echo "Table MyGuests created successfully";
 } else {
-    echo "Error creating table: " . $conn->error;
+    echo "Error creating table: " . $conn->error . "\n";
 }
 
 $conn->close();
