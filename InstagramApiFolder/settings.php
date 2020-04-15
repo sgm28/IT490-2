6a3108fd217d57c0afd947772318d813ff590a9b
Moustapha001 <60150830+Moustapha001@users.noreply.github.com>
2020-04-15 12:35:04 -0400

    Add files via upload

---
+++ InstagramApiFolder/settings.php
@@ -0,0 +1,8 @@
+<?php
+   
+  
+	define( 'CLIENT_ID', '853404941837918');
+	define( 'CLIENT_SECRET', 'ba5748aa8ac8acc658d1417e4b06e5ae');
+	define( 'REDIRECT_URI', 'https://moustapha001.github.io/it490IG.github.io/Login.html' );
+	define( 'ACCESS_TOKEN', 'IGQVJXZAmRsdktrSkJRS1JSX29yd1hfdTMwZAUg5TmYwSHJpOWVUZAjVNY3BhWkxQeGZACV3RRTmF1X1ZAmZAndsV3VwWDNvaUJMamx2dE9HbHJCZAWNHVndVNGdLQmQ4UHowdUZASSFJoMXpMSm9PR2VwQU40eAZDZD' );
+?>
