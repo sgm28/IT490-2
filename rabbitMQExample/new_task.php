331aa1424e3ef260b314c3174875d022f37f3d96
Sakar <sakar.michel@gmail.com>
2020-04-22 23:15:39 +0000

    rabbitmq example 2 creating new task

--- rabbitMQExample/new_task.php
+++ rabbitMQExample/new_task.php
@@ -1,10 +1,16 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+$a = '/../frontEnd/IT490/vendor/autoload.php';
+echo "the path is: ".$a."\n";
+require_once __DIR__ . "$a";
+
+//~/IT490-2/frontEnd/IT490/vendor/autoload.php
+
+//require_once('~/IT490-2/frontEnd/IT490/vendor/autoload.php'); 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConneciton('52.176.58.43',5672,'APP', '1');
+$connection = new AMQPStreamConnection('52.176.58.43',5672,'app', '1');
 $channel = $connection->channel();
 
 
@@ -16,7 +22,7 @@ if (empty($data))
 	$data = "Hello World!";
 }
 
-$msg = new AMQPMesssage($data);
+$msg = new AMQPMessage($data);
 
 $channel->basic_publish($msg, '', 'hello');
 
